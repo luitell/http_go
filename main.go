aad40ad9f0dc0731c0f44fb65362c134006b6571
indiedev02 <dev.kunalshar0@gmail.com>
2026-05-03 01:39:44 +0530

    implemented printing by line

--- main.go
+++ main.go
@@ -3,25 +3,46 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"strings"
 )
 
+const inputFilePath = "messages.txt"
+
 func main() {
-	file, err := os.Open("messages.txt")
+	file, err := os.Open(inputFilePath)
 	if err != nil {
-		fmt.Println("error reading file", err)
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
 	defer file.Close()
+
+	fmt.Printf("Reading data from file\n")
+	fmt.Printf("======================")
 	buffer := make([]byte, 8)
+	currentLine := ""
 
 	for {
 		n, err := file.Read(buffer)
 		if err != nil {
+			if currentLine != "" {
+				fmt.Printf("read: %s\n", currentLine)
+				currentLine = ""
+			}
 			if err != io.EOF {
 				break
 			}
 			return
 		}
-		fmt.Printf("read: %s\n", buffer[:n])
+
+		currentChunk := string(buffer[:n])
+		splitSections := strings.Split(currentChunk, "\n")
+
+		for i := 0; i < len(splitSections)-1; i++ {
+			fmt.Printf("read: %s%s\n", currentLine, splitSections[i])
+			currentLine = ""
+		}
+		currentLine += splitSections[len(splitSections)-1]
+
 	}
 }
